ee2985030aa225d7c9cb94bd5a9803b6740268d4
swaiH <100418655+Sethubhungane@users.noreply.github.com>
2022-09-15 16:21:04 +0200

    Update XOXO.java

--- XOXO.java
+++ XOXO.java
@@ -1,7 +1,22 @@
 public class XOXO {
-    /**
-    Put your methods here
-     */
+    public static void gameboard()
+    {
+	char[][] board = {{' ', '|', ' ', '|', ' '},
+					  {'_', ' ', '_', ' ', '_'}, 
+					  {' ', '|', ' ', '|', ' '}, 
+		              {'_', ' ', '_', ' ', '_'}, 
+				      {' ', '|', ' ', '|', ' '}};
+		
+	for(char[] row:board)   //for each array in the board
+    {	
+		for(char character: row)    //for loop that loops for each character in each row
+        {	
+			System.out.print(character);	//print out each character
+	    }
+		System.out.println();	//prints a line after each row
+			
+	}
+	}
 
     
 }
