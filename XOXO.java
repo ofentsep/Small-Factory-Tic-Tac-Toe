081bea91df92d55c867f4d60da4e5057f2fcddf0
swaiH <100418655+Sethubhungane@users.noreply.github.com>
2022-09-17 16:04:12 +0200

    Update XOXO.java

--- XOXO.java
+++ XOXO.java
@@ -23,59 +23,57 @@ public class XOXO {
             return true;
             }
             return false;
-		}//moveValidation	
-	
-	public static void Place(String[] Grid, int position){ //Thandolwethu
-		
-	/** This method is for placing the player's input.*/
-		
-		if(position >= 1 && position <= 9){ //To check whether the user input is within  the range of the availalbe places to ply at.
-			
-			switch(position){
-				
-				case 1: Grid[0] = "X"; // 
-						break;
-				
-				case 2: Grid[1] = "X";
-						break;
-				
-				case 3: Grid[2] = "X";
-						break;
-				
-				case 4: Grid[3] = "X";
-						break;
-				
-				case 5: Grid[4] = "X";
-						break;
-				
-				case 6: Grid[5] = "X";
-						break;
-				
-				case 7: Grid[6] = "X";
-						break;
-				
-				case 8: Grid[7] = "X";
-						break;
-				
-				case 9: Grid[8] = "X";
-						break;
-			}
-		}
-		else{
-			System.out.println("The position entered is not valid in this board game.");
-			//break;
-		}
-		
-		System.out.println(Arrays.toString(Grid));
-		GridBoard(Grid);
-		
-	public static void userInput(){ // Thandolwethu
-		
-		/**This method is for asking an input from the player.*/
-		
-		System.out.print("Which position you want to play at? : ");
-		
-		int position = in.nextInt() //This is where the player input is assigned to.
+		}//moveValidation
+    
+    public static boolean X_wins(board){
+	if(board[0][0] = 'X' && board[0][2] = 'X' && board[0][4] = 'X'){	//checks if there are three X in the first row going towards right
+		return True	//if true, X wins
+	}else{
+		return False
 	}
+	
+	if(board[2][0] = 'X' && board[2][2] = 'X' && board[2][4] = 'X'){	//checks if there are three X in the second row going towards the right
+		return True	//if true, X wins
+	}else{
+		return False
+	}
+				
+	if(board[4][0] = 'X' && board[4][2] = 'X' && board[4][4] = 'X'){	//checks if there are three X in the third row going towards the right
+		return True	//if true, X wins
+	}else{
+		return False
+	}
+			
+	if(board[0][0] = 'X' && board[2][0] = 'X' && board[4][0] = 'X'){	//checks if there are three X in the first coloumn going down
+		return True	//if true, X wins
+	}else{
+		return False
+	}
+				
+	if(board[0][2] = 'X' && board[2][2] = 'X' && board[4][2] = 'X'){	//checks if there are three X in the second coloumn going down
+		return True	//if true, X wins
+	}else{
+		return False
+			}
+				
+	if(board[0][4] = 'X' && board[2][4] = 'X' && board[4][4] = 'X'){	//checks if there are three X in the third coloumn going down
+		return True	//if true, X wins
+	}else{
+		return False
+			}
+		
+	if(board[0][0] = 'X' && board[2][2] = 'X' && board[4][4] = 'X'){	//checks if there are three X diagonally starting from the top left corner to the bottom right corner
+		return True	//if true, X wins
+	}else{
+		return False
+	}
+		
+	if(board[0][4] = 'X' && board[2][2] = 'X' && board[4][0] = 'X'){	//checks if there are three X diagonally starting from the top right corner to the bottom left corner
+		return True	//if true, X wins
+	}else{
+		return False
+			}
+				
+	}//X winning conditions
 		
 }//XOXO class
