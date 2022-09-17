b15e241bc4b35cf26bd2bd342ccb3b18a96f81db
Prince <princelebudi888@gmail.com>
2022-09-17 10:58:35 +0200

    Added my moveValidation method and did a little editing

--- XOXO.java
+++ XOXO.java
@@ -1,22 +1,28 @@
 public class XOXO {
-    public static void gameboard()
-    {
-	char[][] board = {{' ', '|', ' ', '|', ' '},
-			  {'_', ' ', '_', ' ', '_'}, 
-			  {' ', '|', ' ', '|', ' '}, 
-		          {'_', ' ', '_', ' ', '_'}, 
-			  {' ', '|', ' ', '|', ' '}};
-		
-	for(char[] row:board)   //for each array in the board
-    {	
-		for(char character: row)    //for loop that loops for each character in each row
-        {	
-			System.out.print(character);	//print out each character
-	    }
-		System.out.println();	//prints a line after each row
-			
-	}
-	}
 
+    public static void gameboard(){
+
+    char[][] board = {{' ', '|', ' ', '|', ' '},
+                      {'_', ' ', '_', ' ', '_'}, 
+                      {' ', '|', ' ', '|', ' '}, 
+                      {'_', ' ', '_', ' ', '_'}, 
+                      {' ', '|', ' ', '|', ' '}};
+                        
+	for(char[] row:board){//for each array in the board
+		for(char character: row){//for loop that loops for each character in each row
+			System.out.print(character);	//print out each character
+	        }
+		    System.out.println();//prints a line after each row
+		}//end of loop
     
-}
+	}//gameBoard
+
+    public static boolean moveValidation (int moveidxRow, int moveidxCol, char[][] board2D){
+        char position = board2D[moveidxRow][moveidxCol];
+        if(position == '_' || position == ' '){//If space on board is unoccupied
+            return true;
+            }
+            return false;
+		}//moveValidation	
+		
+}//XOXO class
