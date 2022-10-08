05b0d72c3765a3925b0f699861ad458f09be3cbc
Priince888 <100425934+Priince888@users.noreply.github.com>
2022-10-08 16:36:32 +0200

    Add files via upload

---
+++ XOXO (Classes version)/XOXO.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class XOXO {
+	/*this class is responsible for additional methods not related
+	 *to the board directly */
+
+    public boolean moveValidation (char[][] board, int[] positions){
+		/**This method will validate whether the specified
+		 *position is filled or not
+		 
+		 @param interger-array "positions"
+		 @param 2d-array "board2D"
+		 
+		 @return boolean
+		*/
+		
+        char position = board[positions[0]][positions[1]];
+        if(position == ' ') { //If space on board is unoccupied
+            System.out.println("Empty");
+			return true;
+            }
+			System.out.println("Occupied");
+            return false;
+		}//moveValidation
+		
+}//XOXO class
